d498ee4341511957e5c67de9d79de8d2121119df
Gabe <36547609+gaberaymondi@users.noreply.github.com>
2020-02-02 22:53:47 -0700

    Update

---
+++ scrabbleQ_demo/scrabbleQ_demo/SearchResultsController.swift
@@ -0,0 +1,59 @@
+//
+//  SearchResultsController.swift
+//  scrabbleQ_demo
+//
+//  Created by Gabe Raymondi on 1/30/20.
+//  Copyright © 2020 Gabe Raymondi. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsController: UITableViewController, UISearchResultsUpdating {
+    
+    var allWords = [String]()
+    var filteredWords = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ScrabbleCell")
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return filteredWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ScrabbleCell", for: indexPath)
+    
+        cell.textLabel?.text = filteredWords[indexPath.row]
+        
+        return cell
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        //get user entry
+        let searchString = searchController.searchBar.text
+        
+        filteredWords.removeAll()
+        
+        if searchString?.isEmpty == false {
+            
+            //closure to filter allWords
+            let searchFilter: (String) -> Bool = {(word) in
+                let range = word.range(of: searchString!, options: .caseInsensitive)
+                
+                return range != nil
+            }
+            
+            filteredWords = allWords.filter(searchFilter)
+            
+        }
+        
+        tableView.reloadData()
+    }
+
+}
