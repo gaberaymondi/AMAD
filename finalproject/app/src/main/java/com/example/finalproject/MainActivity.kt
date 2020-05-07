aa447dec3ef1658d86641782b06d570e17f8a613
Gabe <36547609+gaberaymondi@users.noreply.github.com>
2020-05-07 03:58:23 -0600

    epic fail

--- finalproject/app/src/main/java/com/example/finalproject/MainActivity.kt
+++ finalproject/app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    private static final String TAG = "MainActivity"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
